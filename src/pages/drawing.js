a68cc6538fca2079004664c52b800d4932a1844a
DanyaYashin <48764147+DanyaYashin@users.noreply.github.com>
2022-02-28 17:34:34 +0400

    drawing points

--- src/pages/drawing.js
+++ src/pages/drawing.js
@@ -5,11 +5,50 @@ import {Stage, Layer, Circle} from 'react-konva';
 
 export default function Home(props){
 
-  const [inputNumber, setInputNumber] = React.useState("XYZ")
+  function generateShapes() {
+  return [...Array(10)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+const INITIAL_STATE = generateShapes();
+
+
+  const [inputNumber, setInputNumber] = React.useState("coefficient XYZ")
   const [inputValue, setInputValue] = React.useState()
   const [hoverClass, setHoverClass] = React.useState(styles.invis)
   const [formatFile, setFormatFile] = React.useState('lammps')
   const [hideInput, setHideInput] = React.useState(styles.nonecoef)
+  const [points, setPoints] = React.useState(INITIAL_STATE);
+
+    const handleDragStart = (e) => {
+      const id = e.target.id();
+      setPoints(
+        points.map((point) => {
+          return {
+            ...point,
+            isDragging: point.id === id,
+          };
+        })
+      );
+    };
+    const handleDragEnd = (e) => {
+      setPoints(
+        points.map((point) => {
+          return {
+            ...point,
+            isDragging: false,
+          };
+        })
+      );
+    };
+
+
+
   const pressEnter = (e) =>{
     if (e.keyCode === 13){
       let xyz = document.getElementById('xyzInput').value
@@ -42,14 +81,31 @@ export default function Home(props){
     setHideInput(styles.nonecoef)
     setFormatFile(format)
   }
-  
+
   return(
     <Layout>
       <div className={styles.draw}>
         <div className={styles.rectangle}>
-          <Stage width = {100} height={100}>
+          <Stage width={1000} height={1000}>
             <Layer>
-              <Circle x={20} y={20} radius={50} fill="green" />
+            {points.map((point) => (
+              <Circle
+                radius = {10}
+                key={points.id}
+                id={points.id}
+                x={points.x}
+                y={points.y}
+                fill="black"
+                draggable
+                rotation={points.rotation}
+                shadowOffsetX={points.isDragging ? 10 : 5}
+                shadowOffsetY={points.isDragging ? 10 : 5}
+                scaleX={points.isDragging ? 1.2 : 1}
+                scaleY={points.isDragging ? 1.2 : 1}
+                onDragStart={handleDragStart}
+                onDragEnd={handleDragEnd}
+              />
+              ))}
             </Layer>
           </Stage>
         </div>
